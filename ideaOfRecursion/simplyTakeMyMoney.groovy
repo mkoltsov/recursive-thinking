6cd89f9fe88afcd3da34af3dbd48aba5ff1b531d
mkoltsov <root@javabean.ru>
2015-03-13 23:47:36 +0100

    applied recursion to a collection

--- ideaOfRecursion/simplyTakeMyMoney.groovy
+++ ideaOfRecursion/simplyTakeMyMoney.groovy
@@ -1,6 +1,6 @@
 def simplyTake() {
 	for (def i=0;i<10;i++) {
-		println("took you money")
+		println("took your money")
 	}
 }
 
@@ -16,3 +16,33 @@ def tookRecursively(int amount) {
 }
 
 tookRecursively(10)
+
+//will implement later after complete understanding of the concept
+def tookViaDivideAndConquer(def amount) {
+	if (amount == 1) {
+		amount--
+		println("took 1")
+	} else {
+		println("recursively took 1, ${amount} left")
+		int ten = 10
+		tookViaDivideAndConquer(amount/ten)
+	}
+}
+
+tookViaDivideAndConquer(100)
+
+def list = ["chef", "chef1","chef2","chef3","chef4","chef5","chef6","chef7","chef8"]
+
+def applyFunctionOnCollection(def coll, def func) {
+	if (coll.size() ==1) {
+		func(coll)
+	} else {
+		func(coll[coll.size()-1])
+		coll.remove(coll.size()-1)
+		applyFunctionOnCollection(coll, func)
+	}
+}
+
+def clos = {_ -> println(_)}
+
+applyFunctionOnCollection(list, clos)
